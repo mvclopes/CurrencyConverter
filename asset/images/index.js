86c1e48c0adbb6b794bac962c0f43599f839987d
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-02-27 14:03:42 -0300

    Adicionada imagem para background da tela inicial

--- asset/images/index.js
+++ asset/images/index.js
@@ -1,1 +1,2 @@
 export const MONEY_EXCHANGE_ICON = require('./money_exchange.png');
+export const MONEY_BACKGROUND = require('./money_background.jpg');
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,45 +1,50 @@
 import { useState } from "react";
-import { Button, Image, SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Button, Image, ImageBackground, SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 
 // Components
 import AmountInputField from "../../components/AmountInputField";
-import { MONEY_EXCHANGE_ICON } from "../../../asset/images";
+import { MONEY_BACKGROUND, MONEY_EXCHANGE_ICON } from "../../../asset/images";
 
 export default function Home() {
     const [fromCurrency, setFromCurrency] = useState('')
     const [toCurrency, setToCurrency] = useState('')
 
     return(
-    <SafeAreaView>
-        <AmountInputField
-            onChangeText={setFromCurrency}
-            value={fromCurrency}
-            placeholder='Moeda a ser convertida'
-        />
+        <ImageBackground style={styles.container} source={MONEY_BACKGROUND}>
+            <SafeAreaView>
+                <AmountInputField
+                    onChangeText={setFromCurrency}
+                    value={fromCurrency}
+                    placeholder='Moeda a ser convertida'
+                />
 
-        <TouchableOpacity onPress={() => console.log('Change currency')}>
-            <Image style={styles.image} source={MONEY_EXCHANGE_ICON}/>
-        </TouchableOpacity>
-        
-        <AmountInputField
-            onChangeText={setToCurrency}
-            value={toCurrency}
-            placeholder='Moeda convertida'
-        />
+                <TouchableOpacity onPress={() => console.log('Change currency')}>
+                    <Image style={styles.image} source={MONEY_EXCHANGE_ICON}/>
+                </TouchableOpacity>
+                
+                <AmountInputField
+                    onChangeText={setToCurrency}
+                    value={toCurrency}
+                    placeholder='Moeda convertida'
+                />
 
-        <View style={styles.containerButton}>
-            <Button
-                onPress={() => console.log(`from: ${fromCurrency} - to: ${toCurrency}`)}
-                title="Converter moeda"
-                color="#841584"
-                accessibilityLabel="Botão de converter moedas"
-            />
-        </View>
-    </SafeAreaView>
+                <View style={styles.containerButton}>
+                    <Button
+                        onPress={() => console.log(`from: ${fromCurrency} - to: ${toCurrency}`)}
+                        title="Converter moeda"
+                        color="#841584"
+                        accessibilityLabel="Botão de converter moedas"
+                    />
+                </View>
+            </SafeAreaView>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
     containerButton: {
         marginHorizontal: 16,
     },
