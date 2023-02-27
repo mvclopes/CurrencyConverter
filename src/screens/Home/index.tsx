5379be7cca904909860393484ef65159c43b8978
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-02-27 19:08:01 -0300

    Criação inicial de lógica de conversão de moeda

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
-import { Button, Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 // Components
 import AmountInputField from "../../components/AmountInputField";
 import { MONEY_BACKGROUND, MONEY_EXCHANGE_ICON } from "../../../asset/images";
 
 export default function Home() {
-    const [fromCurrency, setFromCurrency] = useState<number>()
-    const [toCurrency, setToCurrency] = useState<number>()
+    const [fromCurrency, setFromCurrency] = useState<number>(0)
+    const [toCurrency, setToCurrency] = useState<number>(0)
+
+    function convertFromAmount(value: string) {
+        try {
+            var amount = parseFloat(value);
+            setFromCurrency(amount >= 0 ? amount : 0);
+            setToCurrency(amount >= 0 ? amount * 5 : 0);
+            console.log(`value: ${value} - amount:${amount} - fromCurrency: ${fromCurrency} - toCurrency: ${toCurrency}`);
+        } catch (error) {
+            console.error(error);
+            zeroStates();
+        }
+    }
+
+    function convertToAmount(value: string) {
+        try {
+            var amount = parseFloat(value);
+            setFromCurrency(amount >= 0 ? amount / 5: 0);
+            setToCurrency(amount >= 0 ? amount : 0);
+            console.log(`value: ${value} - amount:${amount} - fromCurrency: ${fromCurrency} - toCurrency: ${toCurrency}`);
+        } catch (error) {
+            console.error(error);
+            zeroStates();
+        }
+    }
+
+    function zeroStates() {
+        setFromCurrency(0);
+        setToCurrency(0);
+    }
 
     return(
         <ImageBackground blurRadius={4} style={styles.container} source={MONEY_BACKGROUND}>
@@ -15,29 +44,22 @@ export default function Home() {
                 <Text style={styles.title}>Conversor de moedas</Text>
 
                 <AmountInputField
-                    onChangeText={setFromCurrency}
-                    value={fromCurrency}
+                    onChangeText={(value:string) => convertFromAmount(value)}
+                    value={fromCurrency.toString()}
                     placeholder='Moeda a ser convertida'
+                    defaultValue="0"
                 />
 
                 <TouchableOpacity onPress={() => console.log('Change currency')}>
                     <Image style={styles.image} source={MONEY_EXCHANGE_ICON}/>
                 </TouchableOpacity>
-                
-                <AmountInputField
-                    onChangeText={setToCurrency}
-                    value={toCurrency}
-                    placeholder='Moeda convertida'
-                />
 
-                <View style={styles.containerButton}>
-                    <Button
-                        onPress={() => console.log(`from: ${fromCurrency} - to: ${toCurrency}`)}
-                        title="Converter moeda"
-                        color="#841584"
-                        accessibilityLabel="Botão de converter moedas"
-                    />
-                </View>
+                <AmountInputField
+                    onChangeText={(value:string) => convertToAmount(value) }
+                    value={toCurrency.toString()}
+                    placeholder='Moeda convertida'
+                    defaultValue="0"
+                />
             </SafeAreaView>
         </ImageBackground>
     );
@@ -53,6 +75,11 @@ const styles = StyleSheet.create({
         fontSize: 36,
         fontWeight: 'bold',
         marginBottom: 24,
+    },
+    result: {
+        color: '#FF0000',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
     containerButton: {
         marginHorizontal: 16,
