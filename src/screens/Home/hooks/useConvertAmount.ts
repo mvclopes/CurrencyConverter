b4e28f17649a6a47c870150230d051f5f913b9cf
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-03-01 21:36:34 -0300

    Criado hook customizável para conversão de moedas

---
+++ src/screens/Home/hooks/useConvertAmount.ts
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function useConvertAmount() {
+    const [fromCurrency, setFromCurrency] = useState<number>(0)
+    const [toCurrency, setToCurrency] = useState<number>(0)
+
+    function convertFromAmount(value: string) {
+        try {
+            var amount = parseFloat(value);
+            setFromCurrency(amount >= 0 ? amount : 0);
+            setToCurrency(amount >= 0 ? amount * 5 : 0);
+            console.log(`value: ${value} - amount:${amount} - fromCurrency: ${fromCurrency} - toCurrency: ${toCurrency}`);
+        } catch (error) {
+            console.error(error);
+            zeroStates();
+        }
+    }
+
+    function convertToAmount(value: string) {
+        try {
+            var amount = parseFloat(value);
+            setFromCurrency(amount >= 0 ? amount / 5: 0);
+            setToCurrency(amount >= 0 ? amount : 0);
+            console.log(`value: ${value} - amount:${amount} - fromCurrency: ${fromCurrency} - toCurrency: ${toCurrency}`);
+        } catch (error) {
+            console.error(error);
+            zeroStates();
+        }
+    }
+
+    function zeroStates() {
+        setFromCurrency(0);
+        setToCurrency(0);
+    }
+
+    return {
+        fromCurrency: fromCurrency.toString(),
+        toCurrency: toCurrency.toString(),
+        convertFromAmount,
+        convertToAmount
+    }
+}
